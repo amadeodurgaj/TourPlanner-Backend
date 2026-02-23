c37226d0d7974f6933d6deab062c49d3794d14c2
Amadeo Durgaj <thundahlovesjojo@gmail.com>
2026-02-23 18:22:38 +0100

    Initial setup

---
+++ tourplanner/src/main/java/org/example/tourplanner/TourplannerApplication.java
@@ -0,0 +1,13 @@
+package org.example.tourplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourplannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourplannerApplication.class, args);
+    }
+
+}
---
+++ tourplanner/src/test/java/org/example/tourplanner/TourplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tourplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourplannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
